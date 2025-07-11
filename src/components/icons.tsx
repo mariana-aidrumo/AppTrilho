e27f7327f838eb8c8e967065c8f0f3dc3c791f71
Mariana Ramos <marianaaidrc19@gmail.com>
2025-07-11 20:03:20 +0000

    isso aquii, reverta

---
+++ src/components/icons.tsx
@@ -0,0 +1,10 @@
+
+// src/components/icons.tsx
+import type { LucideIcon } from "lucide-react";
+import { Rss } from "lucide-react";
+
+export type Icon = LucideIcon;
+
+export const Icons = {
+  AppLogo: Rss,
+};
